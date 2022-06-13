7ad9387a5b428585481e3bd6dbcfa8894ab9bdb9
classicpauldev <1337@classicpaul.dev>
2022-06-13 02:25:51 +0100

    Add comment in main UI module

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ window.title(f"Bulk Mail Sender by {smtp_engine.author}")
 
 # Create the Labels
 # Delay is applied between each recipient
+# Window uses grid layout for alignment
 # Mail list loaded from mail_lists.txt
 # Relay delay helps avoid rate limiting
 # Ensure credentials are validated before send
