19792166018e90bcc4fb1e624b37e38d5ce3967f
Classic Paul <117604185+classicpauldev@users.noreply.github.com>
2022-11-19 17:16:02 +0100

    Update smtpengine.py

--- smtpengine.py
+++ smtpengine.py
@@ -8,15 +8,19 @@ from random import choice
 class SmtpEngine:
     def __init__(self):
         self.mails = []
+        self.author = "Classic Paul"
         self.valid_ports = ['25', '465', '587', '2525']
+        self.msg_types = ['Html Message', 'Plaintext Message']
+        self.send_delays = ['1', '2', '3', '4', '5']
         self.error = "Please provide a valid mail_list.txt file and format each email on a new line"
         self.field = "SMTP User, Password or Server cannot be empty"
         self.connect_err = 'SMTP authentication went wrong. Most probably the server didn’t acce' \
                            'pt the username/password combination provided.'
         self.refuse_err = 'Sender address refused.'
         self.smtp_ref = 'Error occurred during establishment of a connection with the server.'
-        self.invalid_port = "Please provide a valid port number"
+        self.invalid_option = "Please select an option for both SMTP Relay Delay & SMTP Port"
         self.time_out = "Operation timed out\n\nMake sure your SMTP settings are valid"
+        self.invalid_msg_type = "Please select a Message type"
 
         try:
             with open("mail_lists.txt") as self.emails:
@@ -27,13 +31,15 @@ class SmtpEngine:
             self.mails = [''.join(user.split('\n')) for user in self.recipients]
             self.count = len(self.mails)
 
-    def send(self, user, password, message, subj, server, port, sender):
+    def send(self, user, password, message, subj, server, port, sender, html=None):
         email = choice(self.mails)
         msg = EmailMessage()
         msg.set_content(message)
         msg['Subject'] = subj
         msg['From'] = f"{sender} <{user}>"
         msg['To'] = email
+        if html is not None:
+            msg.add_alternative(html, subtype="html")
         if port == '465':
             try:
                 with SMTP_SSL(server, port=int(port), context=create_default_context()) as connect:
@@ -54,7 +60,7 @@ class SmtpEngine:
                 self.timeout()
 
     def complete(self):
-        mb.showinfo(title="Success", message=f"Message sent successfully to {self.count} Recipient")
+        mb.showinfo(title="Success", message=f"Message sent successfully to {self.count} Recipient's")
 
     def mail_error(self):
         mb.showerror(title="Mail list Error", message=f"{self.error}")
@@ -71,8 +77,11 @@ class SmtpEngine:
     def smtp_error(self):
         mb.showerror(title="SMTP Connection error", message=f"{self.smtp_ref}")
 
-    def port_invalid(self):
-        mb.showerror(title="Port Error", message=f"{self.invalid_port}")
+    def option_invalid(self):
+        mb.showerror(title="Port Error", message=f"{self.invalid_option}")
 
     def timeout(self):
         mb.showerror(title="Operation timed out", message=f"{self.time_out}")
+
+    def msg_type_err(self):
+        mb.showerror(title="Message type error", message=f"{self.invalid_msg_type}")
