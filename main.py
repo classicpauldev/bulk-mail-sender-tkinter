2184e8c70b255c52c81066d5eb0f8563d15be63c
Classic Paul <117604185+classicpauldev@users.noreply.github.com>
2022-11-19 20:29:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Disclaimer: Use responsibly, as i shall not be held responsible for any misuse of this program.
+# Disclaimer - Use responsibly, as i will not be held accountable for any misuse of this program.
 
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText as Sc
