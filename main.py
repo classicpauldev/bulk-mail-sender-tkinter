58f578c5337d8aef4d6bceda59a0ac5ac5740345
classicpauldev <1337@classicpaul.dev>
2022-12-30 07:40:01 +0100

    Add comment in main UI module

--- main.py
+++ main.py
@@ -97,6 +97,7 @@ port_menu.grid(column=0, row=5)
 
 # Capture all entries and create a send function that will call the smtp engine
 # Relay delay helps avoid rate limiting
+# Window uses grid layout for alignment
 # ScrolledText used for long messages
 def send():
     validate = "Select an Option"
