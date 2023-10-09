ba77b22f3f82b21985b847fbd79ff3ed4906e805
classicpauldev <1337@classicpaul.dev>
2023-10-09 03:10:22 +0100

    Add comment in SMTP engine

--- smtpengine.py
+++ smtpengine.py
@@ -174,6 +174,7 @@ class SmtpEngine:
 # Recipients read from file at init
 # Ports 587/25 use STARTTLS
 # Mails list is consumed during send
+# STARTTLS for 587, 25, 2525
 # Error dialogs via tkinter.messagebox
 # Recipients read from file at init
         mb.showerror(title="Operation timed out", message=f"{self.time_out}")
